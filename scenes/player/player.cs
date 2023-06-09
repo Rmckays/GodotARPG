02f0caa26e08d66ca538f60e48fe153c460c08c3
Rmckays <rmckays@gmail.com>
2023-06-09 08:05:05 -0700

    Initial commit

---
+++ scenes/player/player.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody2D
+{
+    [Export] public int Speed = 10;
+    private AnimationPlayer _animationPlayer; 
+    
+	public void HandleInput()
+    {
+        var movementDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        Velocity = movementDirection * Speed;
+    }
+
+    public override void _Ready()
+    {
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        HandleInput();
+        MoveAndSlide();
+        UpdateAnimation();
+        base._PhysicsProcess(delta);
+    }
+
+    private void UpdateAnimation()
+    {
+        if (Velocity.Length() == 0)
+        {
+            _animationPlayer.Stop();
+        }
+        else
+        {
+            var direction = "Down";
+            if (Velocity.X < 0)
+            {
+                direction = "Left";
+            }
+            else if (Velocity.X > 0)
+            {
+                direction = "Right";
+            }
+            else if (Velocity.Y < 0)
+            {
+                direction = "Up";
+            }
+            
+            _animationPlayer.Play("walk" + direction);
+        }
+    }
+}
